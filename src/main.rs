bd59b5c428631bd9c3e0f65469f89870bc8ecf10
Demari Miller <demarijmiller@gmail.com>
2023-06-04 11:09:18 -0400

    New

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing the arguments: {}", err);
+        process::exit(1)
+    });
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("Pulling out the contents {}", contents);
 }
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+
+fn new(args: &[String]) -> Result<Config, &str> {
+
+    if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+    let query: String = args[1].clone();
+    let filename: String = args[2].clone();
+
+    Ok(Config {query, filename})
+}
+}
+
 
